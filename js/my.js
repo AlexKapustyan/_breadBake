391f3a92f63c04bda01c9a9b2c89522614732fe5
AlexKapustyan <55390501+AlexKapustyan@users.noreply.github.com>
2021-09-13 22:50:51 +0300

    Update my.js

--- js/my.js
+++ js/my.js
@@ -13,5 +13,19 @@ $(document).ready(function () {
 		$('.navbar-collapse').removeClass('show');
 		$('.navbar').removeClass('darck');
 	});
+	var scrolled;
+	window.onscroll = function () {
+		scrolled = window.pageYOffset || document.documentElement.scrollTop;//===changing the background and padding of the header when scrolling the page
+		if (scrolled > 500) {
+			$(".navbar").css({ "background": "rgba(0, 0, 0, 0.521)" })
+			$("header .container").css({ "max-width": "85%" })
+			$("header .container").css({ "padding": "1rem 20rem" })
+		}
+		if (500 > scrolled) {
+			$(".navbar").css({ "background": "rgba(0, 0, 0, 0)" })
+			$("header .container").css({ "max-width": "960px" })
+			$("header .container").css({ "padding": "4.0625rem 0" })
+		}
+	}
 });
 
