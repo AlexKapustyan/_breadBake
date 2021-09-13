969fb3386ea4014408712e2438d092c89707d680
AlexKapustyan <55390501+AlexKapustyan@users.noreply.github.com>
2021-09-13 21:32:31 +0300

    Update my.js

--- js/my.js
+++ js/my.js
@@ -1,2 +1,12 @@
+$(document).ready(function () {
 
+	$('.navbar-toggler').click(function () { //===change the background of the dropdown navigation menu
+		$('.navbar').toggleClass('darck');
+	});
+
+	$('.navbar-nav a').click(function () { //===change the background of the dropdown navigation menu
+		$('.navbar-collapse').removeClass('show');
+		$('.navbar').removeClass('darck');
+	});
+});
 
