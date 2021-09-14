0261589a4243d1f2f2d20c4fe3a5b260fc0a4b16
AlexKapustyan <55390501+AlexKapustyan@users.noreply.github.com>
2021-09-14 18:13:20 +0300

    Add files via upload

--- js/my.js
+++ js/my.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
 
 	$('.navbar-toggler').click(function () { //===change the background of the dropdown navigation menu
-		$('.navbar').toggleClass('darck');
+		$('.fixed-top').toggleClass('darck');
 	});
+
+	// $('.navbar-toggler').click(function () { //===change the background of the dropdown navigation menu
+	// 	$('.navbar').toggleClass('fixed-top');
+
+	// });
 
 	$('.navbar-nav a').click(function () { //===remove the background and hide the drop-down menu by clicking on the navigation link
 		$('.navbar-collapse').removeClass('show');
@@ -19,21 +24,21 @@ $(document).ready(function () {
 		scrolled = window.pageYOffset || document.documentElement.scrollTop;//===changing the background and padding of the header when scrolling the page
 		if (scrolled > 50) {
 			$(".navbar").css({ "background": "rgba(0, 0, 0, 0.521)" });
-			$("header .container").css({ "max-width": "100%" });
-			$("header .container").css({ "padding": "1rem 1.8rem" });
+			$("header .container").css({ "max-width": "1600px" });
+			$("header .container").css({ "padding": "1rem 0.9375rem" });
 		}
 		if (50 > scrolled) {
 			$(".navbar").css({ "background": "rgba(0, 0, 0, 0)" });
-			$("header .container").css({ "max-width": "1240px" });
-			$("header .container").css({ "padding": "4.0625rem 1.8rem" });
+			$("header .container").css({ "max-width": "1140px" });
+			$("header .container").css({ "padding": "4.0625rem 0.9375rem" });
 		}
-		if (50 > scrolled || 'max-width: 991px') {
-			$("header .container").css({ "padding": "1rem 1.8rem" });
-			
+		var widthWind = $(this).width();
+		if (widthWind <= 991) {
+			$("header .container").css({ "padding": "1rem 0.9375rem" });
 		}
-
-
 	}
+
+
 
 });
 
